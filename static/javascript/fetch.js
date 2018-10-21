6d52da431e32a1812c555f805d86598c728eb292
Shivam Nagpal <shivamnagpal432@gmail.com>
2018-10-21 12:29:29 +0530

    test

--- static/javascript/fetch.js
+++ static/javascript/fetch.js
@@ -1,22 +1,92 @@
-const url = 'https://gist.githubusercontent.com/ShivamNagpal/d7c37c7e2c87914663dec6df12d57aff/raw/e349b124cfbdd57ea39fdb1ee3680912661e6f8d/Sample%2520Response';
+const iurl = 'https://raw.githubusercontent.com/vtucs/Index_v3/master/index.json';
 
 let xmlhttp = new XMLHttpRequest();
 
-xmlhttp.onreadystatechange = function() {
+xmlhttp.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
         let response = this.responseText;
         let root = JSON.parse(response);
-        for (let i=0;i< root.programs.length;i++) {
-            let prog = root.programs[i];
-            for (let j=0; j<prog.subParts.length; j++) {
-                let sub = prog.subParts[j];
-                for (let k=0; k<sub.files.length; k++) {
-                    let f = sub.files[k];
-
-                }
-            }
+        let laboratoriesDiv = document.getElementById('laboratories-div');
+        for (let i = 0; i < root.laboratories.length; i++) {
+            let eachLabDiv = document.createElement('div');
+            styleDivAsList(eachLabDiv);
+            let lab = root.laboratories[i];
+            let name = document.createTextNode(lab.name);
+            eachLabDiv.appendChild(name);
+            eachLabDiv.addEventListener('click', function () {
+                laboratoryClickHandler(lab.url);
+            });
+            laboratoriesDiv.appendChild(eachLabDiv);
         }
     }
 };
-xmlhttp.open("GET", url, true);
+xmlhttp.open("GET", iurl, true);
 xmlhttp.send();
+
+function laboratoryClickHandler(lurl) {
+    let laboratoryXmlHttp = new XMLHttpRequest();
+    laboratoryXmlHttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let response = this.responseText;
+            let root = JSON.parse(response);
+            let codeDiv = document.getElementById('experiments-div');
+            for (let i = 0; i < root.labExperiments.length; i++) {
+                let programDiv = document.createElement('div');
+                programDiv.setAttribute('class', 'program-div');
+                // programDiv.style.margin = '20px';
+                // programDiv.style.color = 'white';
+                // programDiv.style.backgroundColor = 'black';
+                // programDiv.style.borderRadius = '2px';
+                // programDiv.style.padding = '0.5em';
+                styleDivAsList(programDiv)
+
+                let prog = root.labExperiments[i];
+                for (let j = 0; j < prog.labExperimentSubParts.length; j++) {
+                    let subPartDiv = document.createElement('div');
+                    let sub = prog.labExperimentSubParts[j];
+                    for (let k = 0; k < sub.contentFiles.length; k++) {
+                        let fileDiv = document.createElement('div');
+                        let f = sub.contentFiles[k];
+                        let programTextNode = document.createTextNode(f.title);
+                        fileDiv.appendChild(programTextNode);
+                        fileDiv.addEventListener('click', function () {
+                            experimentClickHandler(f.url)
+                        });
+                        subPartDiv.appendChild(fileDiv);
+
+                    }
+                    programDiv.appendChild(subPartDiv);
+                }
+                codeDiv.appendChild(programDiv);
+            }
+            codeDiv.style.display = 'block';
+            document.getElementById('laboratories-div').style.display = 'none';
+        }
+    };
+    laboratoryXmlHttp.open('GET', lurl, true);
+    laboratoryXmlHttp.send();
+}
+
+function experimentClickHandler(eurl) {
+    let experimentXmlHttp = new XMLHttpRequest();
+    experimentXmlHttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let res = this.responseText;
+            let fetchedPre = document.getElementById('fetched-file-pre');
+            fetchedPre.innerText = res;
+            fetchedPre.style.display = 'block';
+            document.getElementById('experiments-div').style.display = 'none';
+
+        }
+    };
+    experimentXmlHttp.open('GET', eurl, true);
+    experimentXmlHttp.send();
+}
+
+function styleDivAsList(div) {
+    div.style.margin = '20px';
+    div.style.color = 'white';
+    div.style.backgroundColor = '#305D70';
+    div.style.borderRadius = '2px';
+    div.style.padding = '0.5em';
+}
